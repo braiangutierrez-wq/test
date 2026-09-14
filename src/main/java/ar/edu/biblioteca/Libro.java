e61c46c02acbe8000ed56515c31dda245f309356
braiangutierrez-wq <braian.gutierrez@davinci.edu.ar>
2026-09-14 20:13:46 -0300

    feat agregar entidad Libro

---
+++ src/main/java/ar/edu/biblioteca/Libro.java
@@ -0,0 +1,13 @@
+package ar.edu.biblioteca.modelo;
+
+public class Libro{
+    private String isbn;
+    private String titulo;
+    private boolean disponible;
+
+    public Libro(String isbn, String titulo){
+        this.isbn= isbn;
+        this.titulo= titulo;
+        this.disponible = true;
+    }
+}
